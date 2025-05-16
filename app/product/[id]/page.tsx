42e4e6e07c1c3f23b59171b672ad24d28ed7662e
DavyDantas <148136496+DavyDantas@users.noreply.github.com>
2025-05-16 17:31:15 +0000

    update

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,14 +1,25 @@
 "use client"
 import { useParams } from "next/navigation"
+import { useEffect, useState } from "react"
 
-export default function productId (){
-    const {id} = useParams()
+export default function ProductId() {
+    const { id } = useParams()
+    const [product, setProduct] = useState<any>(null)
 
-    return(
+    useEffect(() => {
+        fetch(`https://fakestoreapi.com/products/${id}`)
+        .then(res => res.json())
+        .then(data => {
+            setProduct(data)
+            console.log(data)
+        })
+    }, [id])
+
+    return (
         <main>
             <h3>Produto #{id}</h3>
             <div>
-                <p>Nome: </p>
+                <p>Nome: {product?.title}</p>
             </div>
         </main>
     )
